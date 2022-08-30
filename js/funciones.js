3332bfaf3c16a4ab40c85995bc812c69ec26fb1f
rosmar reyes <rosmarreyes8@gmail.com>
2022-08-29 22:17:17 -0300

    agregando find

--- js/funciones.js
+++ js/funciones.js
@@ -1,34 +1,53 @@
-
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
 let UsuariosQueIngresaron =[];
-let usuario = prompt("Ingrese su nombre");
-function bienvenidaUsuario() {
-    UsuariosQueIngresaron.push(usuario)
-    console.log(UsuariosQueIngresaron);
-    alert("Bienvenido a Quintero Barber " + UsuariosQueIngresaron + ", " + "acontinuacion le indicaremos los horarios disponibles para realizar su reserva");
-}
 
+class BienvenidaUsuarios {
+    constructor (nombre, apellido){
+        this.nombre= nombre;
+        this.apellido= apellido;
+    };
+    
+    bienvenida() {
+        alert("Bienvenido a Quintero Barber " + UsuariosQueIngresaron + ", " + "acontinuacion le indicaremos los horarios disponibles para realizar su reserva");
+        UsuariosQueIngresaron.push(this.nombre, this.apellido);
+        console.log(UsuariosQueIngresaron);
+    }
+}; 
+let usuario= new BienvenidaUsuarios (nombre, apellido);
+
+
+let horarios= [
+    {dia:'lunes', hora: "15:00hs"},
+    {dia:'martes', hora: "9:00hs"},
+    {dia:'miercoles', hora: "13:00hs"},
+    {dia:'jueves', hora: "10:00hs"},
+    {dia:'viernes', hora: "11:00hs"},
+    {dia:'sabado', hora: "18:00hs"},
+];
 function reserva() {
-    let dia = prompt("En Quintero Barber trabajamos de Lunes a Sabado de 9hs a 19hs, indique el dia de la semana que desea asistir: \n * Lunes \n * Martes \n * Miercoles \n * Jueves \n * Viernes \n * Sabado \n o ingrese ESC para salir").toUpperCase();
-
+    let dia = prompt("En Quintero Barber podemos reservar por Dia o por Hora,\nIngrese 'Dia', para ingresar \nIngrese 'Hora' para ingresar \nO ingreses ESC para salir").toUpperCase();
+    
     while (dia !== "ESC" ) {
-        if (dia ==="LUNES"|| dia ==="MARTES"||dia ==="MIERCOLES"|| dia ==="JUEVES"||dia ==="VIERNES"|| dia ==="SABADO" ) {
-                let horario = prompt("los horarios disponibles para el dia " + dia + " son los siguientes: \n 10:00hs \n 13:00hs \n 15:00hs \n 19:00hs \n Ingrese el horario que desea reservar en el formato (00:00hs) o ingrese ATRAS para volver al menu anterior");
-                if (horario === "10:00hs"||horario === "13:00hs"||horario === "15:00hs"||horario === "19:00hs"){
-                    alert(usuario + "," + " Su reserva ha sido agendada con exito para el dia "+ dia + " a las " + horario);
-                    break
-                } else if (horario === "ATRAS") {
-                    return reserva();
-                } else {
-                    alert ("ingrese un dato valido");
-                }      
-        } else if (dia !== (dia ==="LUNES"|| dia ==="MARTES"||dia ==="MIERCOLES"|| dia ==="JUEVES"||dia ==="VIERNES"|| dia ==="SABADO")){
+        if (dia === "DIA" ) {
+            let reservaPorDia = prompt("los dias disponibles son: LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, y SABADO ");
+            let diaReservado = horarios.find(elemento => (elemento.dia === reservaPorDia))
+            alert("Hemos agendado su reserva, para el dia " + diaReservado.dia + " a las " + diaReservado.hora)
+            break
+        } else if(dia === "HORA"){
+            let reservaPorHora = prompt("Los horarios disponibles son: 15:00hs, 9:00hs, 13:00hs, 10:00hs, 11:00hs, 18:00s")
+            let horarioReservado = horarios.find(elemento => (elemento.hora === reservaPorHora))
+            alert("Hemos agendado su reserva, para el dia " + horarioReservado.dia + " a las " + horarioReservado.hora)
+            break
+        }else if(dia !== "HORA" || dia !== "DIA"){
             alert("por favor ingrese un dato valido");
             return reserva();
         }
-    }if (dia === ESC) {
+    if (dia === "ESC") {
         alert("usted ha salido de la seccion de reserva");
     } 
 }
+}
 
-bienvenidaUsuario();
+usuario.bienvenida();
 reserva();
